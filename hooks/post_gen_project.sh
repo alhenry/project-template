22205cef23824642b7be8889bdc14c2c675c6808
Albert <albert_henry@outlook.com>
2022-01-01 16:01:54 +0700

    update template

--- hooks/post_gen_project.sh
+++ hooks/post_gen_project.sh
@@ -4,8 +4,14 @@ GH_username='{{ cookiecutter.GitHub_username }}'
 GH_token='{{ cookiecutter.GitHub_token }}'
 GH_reponame='{{ cookiecutter.GitHub_repo_name }}'
 
-curl -s https://api.github.com/user/repos?access_token=$GH_token \
-  -d '{"name":"'"$GH_reponame"'"}'
+curl \
+  -X POST \
+  -H "Accept: application/vnd.github.v3+json" \
+  https://api.github.com/user/repos \
+  -d '{"name":"name"}'
+
+# curl -s https://api.github.com/user/repos?access_token=$GH_token \
+#   -d '{"name":"'"$GH_reponame"'"}'
 
 cd '{{ cookiecutter.directory_name }}'
 git init
