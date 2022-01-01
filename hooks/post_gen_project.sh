401a17d1af32b365e3404ce79c4fdc3accb5e410
Albert <albert_henry@outlook.com>
2022-01-01 16:34:59 +0700

    update with gh CLI

--- hooks/post_gen_project.sh
+++ hooks/post_gen_project.sh
@@ -8,7 +8,7 @@ curl \
   -X POST \
   -H "Accept: application/vnd.github.v3+json" \
   https://api.github.com/user/repos \
-  -d '{"name":"name"}'
+  -d '{"name":"$GH_reponame", "private":"true"}'
 
 # curl -s https://api.github.com/user/repos?access_token=$GH_token \
 #   -d '{"name":"'"$GH_reponame"'"}'
@@ -17,7 +17,11 @@ cd '{{ cookiecutter.directory_name }}'
 git init
 git add *
 git commit -m "initial commit"
-git remote add origin https://github.com/${GH_username}/${GH_reponame}.git
-git push -u origin master
+
+# git remote add origin https://github.com/${GH_username}/${GH_reponame}.git
+# git push -u origin master
+
+# With GH CLI https://cli.github.com/manual/gh_repo_create
+gh repo create "$GH_reponame" --private --source=. --remote=upstream
 
 cd ..
