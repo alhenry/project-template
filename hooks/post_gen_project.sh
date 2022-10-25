1a94a5af70efcecdae15d4a2741e96eea3da971c
Albert <albert_henry@outlook.com>
2022-10-25 12:27:44 +0100

    edit post_gen_project.sh

--- hooks/post_gen_project.sh
+++ hooks/post_gen_project.sh
@@ -4,24 +4,24 @@ GH_username='{{ cookiecutter.GitHub_username }}'
 GH_token='{{ cookiecutter.GitHub_token }}'
 GH_reponame='{{ cookiecutter.GitHub_repo_name }}'
 
-curl \
-  -X POST \
-  -H "Accept: application/vnd.github.v3+json" \
-  https://api.github.com/user/repos \
-  -d '{"name":"$GH_reponame", "private":"true"}'
+# curl \
+#   -X POST \
+#   -H "Accept: application/vnd.github.v3+json" \
+#   https://api.github.com/user/repos \
+#   -d '{"name":"$GH_reponame", "private":"true"}'
 
 # curl -s https://api.github.com/user/repos?access_token=$GH_token \
 #   -d '{"name":"'"$GH_reponame"'"}'
 
+# With GH CLI https://cli.github.com/manual/gh_repo_create
+gh repo create "$GH_reponame" --private
+
 cd '{{ cookiecutter.directory_name }}'
 git init
-git add *
+git add -A
 git commit -m "initial commit"
-
-# git remote add origin https://github.com/${GH_username}/${GH_reponame}.git
-# git push -u origin master
-
-# With GH CLI https://cli.github.com/manual/gh_repo_create
-gh repo create "$GH_reponame" --private --source=. --remote=upstream
+git branch -M main
+git remote add origin git@github.com:${GH_username}/${GH_reponame}.git
+git push -u origin main
 
 cd ..
